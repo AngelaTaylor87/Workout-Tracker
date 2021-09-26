32dea251d58a48e27d7b587c83efa66a8d95905f
AngelaTaylor87 <82851741+AngelaTaylor87@users.noreply.github.com>
2021-09-25 20:39:43 -0500

    fixed bugs

--- models/workout.js
+++ models/workout.js
@@ -6,12 +6,25 @@ const exerciseSchema = new Schema({
   day: {type: Date, default: Date.now},
   exercises: [
     {
-      type: String,
-      name: String,
-      duration: Number,
-      weight: Number,
-      reps: Number,
-      sets: Number,
+      type: { 
+        type: String 
+      },
+      name: { 
+        type: String
+      },
+      duration: {
+        type: Number 
+      },
+      weight: {
+        type: Number
+       },
+      reps: {
+        type: Number
+       },
+      sets: { 
+        type: Number },
+      distance: { 
+        type: Number }
     },
   ],
 });
